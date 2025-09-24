a590666a1eedaacf68b5c16b8bed3d586b5be20d
moeaberanek <moeaberanek@hotmail.com>
2025-09-24 13:34:01 +0530

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -4,9 +4,9 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('.'))
 
-project = 'Nortton Sure Apps'
+project = 'Lifelock Nort Monitors'
 copyright = '2025'
-author = 'Ellis Axelsson'
+author = 'Alexandra Allred'
 
 release = '1.0'
 
@@ -21,3 +21,4 @@ exclude_patterns = []
 
 html_theme = 'alabaster'
 html_static_path = ['_static']
+
